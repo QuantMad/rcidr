f3a9c74cabc6f402bfa708b6ae8cdfd2ecb74b45
balanyuk.ale <alexandrbalanyukworkmail@gmail.com>
2022-03-10 11:15:14 +0200

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
-use std::net::Ipv4Addr;
-use std::env::args;
-use clap::{Arg, App};
+mod stat;
+
+use clap::{App};
+use crate::stat::*;
 
 fn main() {
-    let app = App::new("test1")
-        .version("0.0.1")
-        .about("Do something")
-        .author("quantmad");
+    let app = App::new("rcidr")
+        .version("v0.0.1")
+        .about("About:\tDo something")
+        .author("Author:\tQuantMad");
 
-
+    let _ = app.get_matches();
 
     let a : u8 = 10; let b : u8 = 88; let c : u8 = 2; let d : u8 = 3;
-    let n = ip4tou32(a, b, c, d);
+    let n = ip4_to_u32(a, b, c, d);
     println!("{}.{}.{}.{} = {}", a, b, c, d, n);
-    let i = u32toip4(n).octets();
+    let i = u32_to_ip4(n).octets();
     println!("{} = {}.{}.{}.{}", n, i[0], i[1], i[2], i[3]);
-    println!("{}", cidr_addr_count(24));
-}
-
-fn cidr_addr_count(prefix:u8) -> u32 {
-    if prefix > 32 {
-        panic!("CIDR prefix can't be greater than 32")
-    }
-    2_u32.pow((32 - prefix) as u32)
-}
-
-fn ip4tou32(a:u8, b:u8, c:u8, d:u8) -> u32 {
-    u32::from(Ipv4Addr::new(a, b, c, d))
-}
-
-fn u32toip4(num:u32) -> Ipv4Addr {
-    Ipv4Addr::from(num)
+    println!("{}",  cidr_addr_count(24));
 }
---
+++ src/stat.rs
@@ -0,0 +1,16 @@
+use std::net::Ipv4Addr;
+
+pub fn cidr_addr_count(prefix:u8) -> u32 {
+    if prefix > 32 {
+        panic!("CIDR prefix can't be greater than 32")
+    }
+    2_u32.pow((32 - prefix) as u32)
+}
+
+pub fn ip4_to_u32(a:u8, b:u8, c:u8, d:u8) -> u32 {
+    u32::from(Ipv4Addr::new(a, b, c, d))
+}
+
+pub fn u32_to_ip4(num:u32) -> Ipv4Addr {
+    Ipv4Addr::from(num)
+}
