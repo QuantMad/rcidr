8ecdc3a729126a3ab1a6f6ff8ea1b6b293dda4d7
balanyuk.ale <alexandrbalanyukworkmail@gmail.com>
2022-03-16 16:11:43 +0200

    match res.export_to

--- src/cidr.rs
+++ src/cidr.rs
@@ -1,5 +1,3 @@
-use crate::stat::*;
-
 use std::fmt::{Debug, Formatter};
 use std::net::Ipv4Addr;
 use std::str::FromStr;
@@ -24,6 +22,25 @@ impl Cidr {
     }
 }
 
+pub fn cidr_addr_count(prefix:u8) -> u32 {
+    if prefix > 32 {
+        panic!("CIDR prefix can't be greater than 32")
+    }
+    2_u32.pow((32 - prefix) as u32)
+}
+
+/*pub fn octets_to_u32(a:u8, b:u8, c:u8, d:u8) -> u32 {
+    u32::from(Ipv4Addr::new(a, b, c, d))
+}*/
+
+pub fn ip4_to_u32(addr:Ipv4Addr) -> u32 {
+    u32::from(addr)
+}
+
+pub fn u32_to_ip4(num:u32) -> Ipv4Addr {
+    Ipv4Addr::from(num)
+}
+
 impl FromStr for Cidr {
     type Err = ParseError;
 
@@ -32,11 +49,14 @@ impl FromStr for Cidr {
             let pare = s.split('/').collect::<Vec<&str>>();
             let base = Ipv4Addr::from_str(pare[0])
                 .expect("Некорректный формат ввода IP адреса");
+
             let prefix = u8::from_str(pare[1])
                 .expect("Некорректный формат ввода префикса");
+
             if prefix > 32 {
                 panic!("Некорректный формат ввода префикса");
             }
+
             Ok(Cidr { base, prefix })
         } else {
             let base = Ipv4Addr::from_str(s)
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod cidr;
-mod stat;
 
 use clap::{Parser};
 use crate::cidr::Cidr;
@@ -10,12 +9,18 @@ fn main() {
     for a in res.cidr.to_vec() {
         println!("{}", a);
     }
+
+    match res.export_to {
+        Some(export_to) => println!("{}", export_to),
+        None => println!("нихуя")
+    }
 }
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(name = "rcidr", author = "QuantMad", version = "0.1.0", about, long_about = None)]
 pub struct Cli {
     #[clap(short = 'n', long = "network", name = "network")]
     cidr: Cidr,
+    #[clap(short = 'e', long = "export", name = "export_to")]
+    export_to:Option<String>,
 }
-
